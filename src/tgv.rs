398dc8e7dd3f439536b87fe80ef3347dddfa08b9
apleynes <19675801+apleynes@users.noreply.github.com>
2025-05-23 23:20:59 +0200

    WIP chunking parallelization of par_azip sections

--- src/tgv.rs
+++ src/tgv.rs
@@ -211,8 +211,10 @@ fn sym_gradient(w: &ArrayView3<f32>) -> Array3<f32> {
     });
 
     let mut off_diagonals = tmp1;
-    par_azip!((x in &mut off_diagonals, &y in &tmp2) {
-        *x = 0.5 * (*x + y);
+    let mut off_diagonals_chunks = off_diagonals.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+    let tmp2_chunks = tmp2.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+    off_diagonals_chunks.zip(tmp2_chunks).par_bridge().for_each(|(mut x, y)| {
+        x.assign(&(0.5 * (x.to_owned() + y)));
     });
 
     ndarray::stack![Axis(2), first_diagonal, second_diagonal, off_diagonals]
@@ -256,8 +258,10 @@ fn sym_divergence(q: &ArrayView3<f32>) -> Array3<f32> {
             col.assign(&diff.map(|x| -0.5 * x));
         });
     });
-    par_azip!((x in &mut first_component, &y in &second_term) {
-        *x += y;
+    let mut first_component_chunks = first_component.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+    let second_term_chunks = second_term.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+    first_component_chunks.zip(second_term_chunks).par_bridge().for_each(|(mut x, y)| {
+        x.assign(&(x.to_owned() + y));
     });
 
     // Second component: ∂y q_1 - ∂x q_2
@@ -294,17 +298,16 @@ fn sym_divergence(q: &ArrayView3<f32>) -> Array3<f32> {
             row.assign(&diff.map(|x| -0.5 * x));
         });
     });
-    par_azip!((x in &mut second_component, &y in &second_term) {
-        *x += y;
+    let mut second_component_chunks = second_component.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+    let second_term_chunks = second_term.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+    second_component_chunks.zip(second_term_chunks).par_bridge().for_each(|(mut x, y)| {
+        x.assign(&(x.to_owned() + y));
     });
 
     ndarray::stack![Axis(2), first_component, second_component]
 }
 
 fn proj_p(p: &ArrayView3<f32>, alpha1: &f32) -> Array3<f32> {
-    // let norm = (p.slice(s![.., .., 0]).map(|x| x.powi(2)) 
-    //     + p.slice(s![.., .., 1]).map(|x| x.powi(2)))
-    //     .sqrt();
     let norm = p.clone().to_owned().map_axis_mut(Axis(2), |slice| {
         slice.powi(2).sum().sqrt()
     });
@@ -318,24 +321,30 @@ fn proj_p(p: &ArrayView3<f32>, alpha1: &f32) -> Array3<f32> {
         }
     });
     let mut p_proj = p.to_owned();
-    let mut slice1 = p_proj.slice_mut(s![.., .., 0]);
-    // slice1 /= &factor;
-    par_azip!((x in &mut slice1, &y in &factor) {
-        *x /= y;
+    
+    // Apply chunked parallelization for slice1
+    let num_cpus = num_cpus::get();
+    let (y_size, _, _) = p_proj.dim();
+    let chunk_size = y_size / num_cpus;
+    let mut sliced_p_proj = p_proj.slice_mut(s![.., .., 0]);
+    let mut slice1_chunks = sliced_p_proj.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+    let factor_chunks = factor.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+    slice1_chunks.zip(factor_chunks).par_bridge().for_each(|(mut x, y)| {
+        x.assign(&(x.to_owned() / &y));
     });
-    let mut slice2 = p_proj.slice_mut(s![.., .., 1]);
-    // slice2 /= &factor;
-    par_azip!((x in &mut slice2, &y in &factor) {
-        *x /= y;
+    
+    // Apply chunked parallelization for slice2
+    let mut sliced_p_proj = p_proj.slice_mut(s![.., .., 1]);
+    let mut slice2_chunks = sliced_p_proj.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+    let factor_chunks = factor.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+    slice2_chunks.zip(factor_chunks).par_bridge().for_each(|(mut x, y)| {
+        x.assign(&(x.to_owned() / &y));
     });
+    
     p_proj
 }
 
 fn proj_q(q: &ArrayView3<f32>, alpha0: &f32) -> Array3<f32> {
-    // let norm = (q.slice(s![.., .., 0]).map(|x| x.powi(2)) 
-    //     + q.slice(s![.., .., 1]).map(|x| x.powi(2)) 
-    //     + q.slice(s![.., .., 2]).map(|x| x.powi(2)))
-    //     .sqrt();
     let norm = q.clone().to_owned().map_axis_mut(Axis(2), |slice| {
         slice.powi(2).sum().sqrt()
     });
@@ -349,21 +358,36 @@ fn proj_q(q: &ArrayView3<f32>, alpha0: &f32) -> Array3<f32> {
         }
     });
     let mut q_proj = q.to_owned();
-    let mut slice1 = q_proj.slice_mut(s![.., .., 0]);
-    // slice1 /= &factor;
-    par_azip!((x in &mut slice1, &y in &factor) {
-        *x /= y;
+    
+    // Apply chunked parallelization for all three slices
+    let num_cpus = num_cpus::get();
+    let (y_size, _, _) = q_proj.dim();
+    let chunk_size = y_size / num_cpus;
+    
+    // Slice 0
+    let mut sliced_q_proj = q_proj.slice_mut(s![.., .., 0]);
+    let mut slice1_chunks = sliced_q_proj.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+    let factor_chunks = factor.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+    slice1_chunks.zip(factor_chunks).par_bridge().for_each(|(mut x, y)| {
+        x.assign(&(x.to_owned() / &y));
     });
-    let mut slice2 = q_proj.slice_mut(s![.., .., 1]);
-    // slice2 /= &factor;
-    par_azip!((x in &mut slice2, &y in &factor) {
-        *x /= y;
+    
+    // Slice 1
+    let mut sliced_q_proj = q_proj.slice_mut(s![.., .., 1]);
+    let mut slice2_chunks = sliced_q_proj.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+    let factor_chunks = factor.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+    slice2_chunks.zip(factor_chunks).par_bridge().for_each(|(mut x, y)| {
+        x.assign(&(x.to_owned() / &y));
     });
-    let mut slice3 = q_proj.slice_mut(s![.., .., 2]);
-    // slice3 /= &factor;
-    par_azip!((x in &mut slice3, &y in &factor) {
-        *x /= y;
+    
+    // Slice 2
+    let mut sliced_q_proj = q_proj.slice_mut(s![.., .., 2]);
+    let mut slice3_chunks = sliced_q_proj.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+    let factor_chunks = factor.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+    slice3_chunks.zip(factor_chunks).par_bridge().for_each(|(mut x, y)| {
+        x.assign(&(x.to_owned() / &y));
     });
+    
     q_proj
 }
 
@@ -428,16 +452,29 @@ pub fn tgv_mri_reconstruction(
     let t: f32 = 1.0;  // Adaptive momentum
     for i in 0..max_iter {
         let grad_u_bar = gradient(&u_bar.view());
-        par_azip!((x in &mut p, &y in &grad_u_bar, &z in &w_bar) {
-            *x += &sigma * (y - z);
+        
+        // Replace par_azip with chunked parallelization
+        let num_cpus = num_cpus::get();
+        let (y_size, _, _) = p.dim();
+        let chunk_size = y_size / num_cpus;
+        let mut p_chunks = p.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+        let grad_chunks = grad_u_bar.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        let w_bar_chunks = w_bar.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        p_chunks.zip(grad_chunks.zip(w_bar_chunks)).par_bridge().for_each(|(mut x, (y, z))| {
+            x.assign(&(x.to_owned() + sigma * (y.to_owned() - z.to_owned())));
         });
+        
         let p = proj_p(&p.view(), &(&alpha0));
-        // log!("Min max: {}, {}", p.clone().into_iter().reduce(f32::min).unwrap(), p.clone().into_iter().reduce(f32::max).unwrap());
 
         let sym_grad_w_bar = sym_gradient(&w_bar.view());
-        par_azip!((x in &mut q, &y in &sym_grad_w_bar) {
-            *x += &sigma * &lambda * y;
+        
+        // Replace par_azip with chunked parallelization
+        let mut q_chunks = q.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+        let sym_grad_chunks = sym_grad_w_bar.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        q_chunks.zip(sym_grad_chunks).par_bridge().for_each(|(mut x, y)| {
+            x.assign(&(x.to_owned() + sigma * lambda * y.to_owned()));
         });
+        
         let q = proj_q(&q.view(), &(&alpha1));
 
         // Primal updates
@@ -446,8 +483,12 @@ pub fn tgv_mri_reconstruction(
 
         // Update u: TGV + data fidelity
         let div_p = divergence(&p.view());
-        par_azip!((x in &mut u, &y in &div_p) {
-            *x -= tau * &lambda * y;
+        
+        // Replace par_azip with chunked parallelization for u update
+        let mut u_chunks = u.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+        let div_p_chunks = div_p.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        u_chunks.zip(div_p_chunks).par_bridge().for_each(|(mut x, y)| {
+            x.assign(&(x.to_owned() - tau * lambda * y.to_owned()));
         });
 
         // Data fidelity term
@@ -461,14 +502,23 @@ pub fn tgv_mri_reconstruction(
         });
         let ifft_residual = ifft2(&ifft2shift(&residual.view()).view());
         let ifft_residual = Array2::<f32>::from_shape_vec((ny, nx), ifft_residual.into_iter().map(|x| x.re as f32).collect()).unwrap();
-        par_azip!((x in &mut u, &y in &ifft_residual) {
-            *x -= tau * y;
+        
+        // Replace par_azip with chunked parallelization for final u update
+        let mut u_chunks = u.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+        let ifft_residual_chunks = ifft_residual.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        u_chunks.zip(ifft_residual_chunks).par_bridge().for_each(|(mut x, y)| {
+            x.assign(&(x.to_owned() - tau * y.to_owned()));
         });
 
         // Update w
         let sym_div_q = sym_divergence(&q.view());
-        par_azip!((x in &mut w, &y in &p, &z in &sym_div_q) {
-            *x -= tau * (-y) * z;
+        
+        // Replace par_azip with chunked parallelization for w update
+        let mut w_chunks = w.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+        let p_chunks = p.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        let sym_div_q_chunks = sym_div_q.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        w_chunks.zip(p_chunks.zip(sym_div_q_chunks)).par_bridge().for_each(|(mut x, (y, z))| {
+            x.assign(&(x.to_owned() - tau * (-y.to_owned() + z.to_owned())));
         });
         
         // Extrapolation
@@ -476,13 +526,21 @@ pub fn tgv_mri_reconstruction(
         let t_old = t;
         let t = (1. + (1. + 4. * t_old.powi(2)).sqrt()) / 2.0;
         let theta = (1. - t_old) / t;
-        par_azip!((x in &mut u_bar, &y in &u, &z in &u_old) {
-            // *x = 2. * y - z;
-            *x = y + theta * (y - z);
+        
+        // Replace par_azip with chunked parallelization for u_bar update
+        let mut u_bar_chunks = u_bar.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+        let u_chunks = u.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        let u_old_chunks = u_old.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        u_bar_chunks.zip(u_chunks.zip(u_old_chunks)).par_bridge().for_each(|(mut x, (y, z))| {
+            x.assign(&(y.to_owned() + theta * (y.to_owned() - z.to_owned())));
         });
-        par_azip!((x in &mut w_bar, &y in &w, &z in &w_old) {
-            // *x = 2. * y - z;
-            *x = y + theta * (y - z);
+        
+        // Replace par_azip with chunked parallelization for w_bar update
+        let mut w_bar_chunks = w_bar.axis_chunks_iter_mut(Axis(0), chunk_size).into_iter();
+        let w_chunks = w.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        let w_old_chunks = w_old.axis_chunks_iter(Axis(0), chunk_size).into_iter();
+        w_bar_chunks.zip(w_chunks.zip(w_old_chunks)).par_bridge().for_each(|(mut x, (y, z))| {
+            x.assign(&(y.to_owned() + theta * (y.to_owned() - z.to_owned())));
         });
 
         let total_residual: f64 = residual.map(|x| x.re.powi(2)).sum();
